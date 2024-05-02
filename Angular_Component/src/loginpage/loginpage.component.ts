f2de709adbc45fac27a6815eeae66fb93163cb4f
Vishal Singh <119868252+VS1477@users.noreply.github.com>
2024-05-02 12:40:05 +0530

    Add files via upload

---
+++ Angular_Component/src/loginpage/loginpage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-loginpage',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './loginpage.component.html',
+  styleUrl: './loginpage.component.css',
+  
+})
+export class LoginpageComponent {
+
+}
